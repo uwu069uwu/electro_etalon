4df981b639da3f3da8e44f164e9d4446a3ba5334
uwu069uwu <mistrsultan0017@gmail.com>
2026-05-09 23:19:41 +0500

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://electroetalon-production.up.railway.app/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://electroetalon-production.up.railway.app/api",
 });
 
 API.interceptors.request.use((config) => {
